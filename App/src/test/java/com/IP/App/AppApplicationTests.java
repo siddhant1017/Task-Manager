e9d2595cc61d75621906829066440df0361f085e
Gopikrishnan <gopikrishnan.rmg@live.in>
2022-10-18 23:59:26 -0400

    Initial Commit

---
+++ App/src/test/java/com/IP/App/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IP.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
