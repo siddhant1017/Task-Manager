b342bebf13f6c01aee64aa79180eaae7bb4dd5fb
Tanya Sethi <sethi.tanya123@gmail.com>
2022-11-20 01:55:52 -0500

    Update PhotoController.java
    Delete api

--- App/src/main/java/com/IP/App/controller/PhotoController.java
+++ App/src/main/java/com/IP/App/controller/PhotoController.java
@@ -2,12 +2,11 @@ package com.IP.App.controller;
 
 import com.IP.App.Models.Photo;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.tomcat.util.json.JSONParser;
 import org.bson.BsonBinarySubType;
 import org.bson.types.Binary;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -21,12 +20,23 @@ public class PhotoController {
 
     @PostMapping(value= "/photos/add",
             consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-    public Photo addPhoto( Photo photo, @RequestParam("image") MultipartFile image) throws IOException {
-       // Photo newImage = new ObjectMapper().readValue(photo, Photo.class);
-        Photo newImage= new Photo();
+    public Photo addPhoto(@RequestParam("photo") String photo, @RequestParam("image") MultipartFile image) throws IOException {
+        Photo newImage = new ObjectMapper().readValue(photo, Photo.class);
+        //Photo newImage= new Photo();
+        //newImage=photo;
         newImage.setImageData(new Binary(BsonBinarySubType.BINARY,image.getBytes()));
 
         newImage = photoRepository.insert(newImage);
         return newImage;
+    }
+
+    @PostMapping(value= "/photos/delete")
+    public ResponseEntity<Photo> deletePhoto( @RequestBody Photo photo) {
+        return photoRepository.findById(photo.getPhotoId())
+                .map(photoData -> {
+                    photoData.setDeleted(photo.getDeleted());
+                    Photo updatedPhoto = photoRepository.save(photoData);
+                    return ResponseEntity.ok().body(updatedPhoto);
+                }).orElse(ResponseEntity.notFound().build());
     }
 }
