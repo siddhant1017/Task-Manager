88b692cf8a8df251766c4222886299d78f67e3c2
Gopikrishnan Rajeev <gopikrishnan.rmg@live.in>
2022-11-20 16:57:53 -0500

    Created APIs for getallphotos and getalldeletedphotos

--- App/src/main/java/com/IP/App/controller/PhotoController.java
+++ App/src/main/java/com/IP/App/controller/PhotoController.java
@@ -4,13 +4,19 @@ import com.IP.App.Models.Photo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.bson.BsonBinarySubType;
 import org.bson.types.Binary;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 public class PhotoController {
@@ -36,5 +42,27 @@ public class PhotoController {
                     Photo updatedPhoto = photoRepository.save(photoData);
                     return ResponseEntity.ok().body(updatedPhoto);
                 }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping(value= "/photos/getallphotos", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getAllPhotos(@RequestBody String request){
+        JSONObject requestObject = new JSONObject(request);
+        JSONArray responseObject = new JSONArray();
+        List photos = photoRepository.getPhotosByLoginID(requestObject.getString("loginId"));
+        for (Object photo: photos)
+            responseObject.put(new JSONObject(photo));
+
+        return responseObject.toString();
+    }
+
+    @PostMapping(value= "/photos/getalldeletedphotos", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getAllDeletedPhotos(@RequestBody String request){
+        JSONObject requestObject = new JSONObject(request);
+        JSONArray responseObject = new JSONArray();
+        List photos = photoRepository.getDeletedPhotosByLoginID(requestObject.getString("loginId"));
+        for (Object photo: photos)
+            responseObject.put(new JSONObject(photo));
+
+        return responseObject.toString();
     }
 }
